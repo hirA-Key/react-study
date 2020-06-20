5331d0ad66f1bc712c070b74ee1358284e5e53ca
kii hira <hira12.tai11@gmail.com>
2020-06-21 06:16:42 +0900

    todolistcomponentの作成

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from './todoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hellow world!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const list = ["買い物", "勉強"];
+
+    return (
+      <div>
+        <h1>ToDoアプリ</h1>
+        <TodoList list={list} />
+      </div>
+    );
+  }
 }
 
 export default App;
