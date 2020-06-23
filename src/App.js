a053e1a0705c2ff7c37c074bf2411dc3f7b88967
kii hira <hira12.tai11@gmail.com>
2020-06-23 22:15:40 +0900

    todo完成

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import TodoList from './todoList';
+import Form from './Form';
 
 class App extends React.Component {
-  render() {
-    const list = ["買い物", "勉強"];
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: ["買い物", "勉強"],
+    };
+  }
 
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    this.setState({
+      list: [
+        ...this.state.list,
+        this.state.value,
+      ],
+      value: '',
+    });
+  }
+
+  render() {
     return (
       <div>
         <h1>ToDoアプリ</h1>
-        <TodoList list={list} />
+        <Form
+          //入力値をstateで保持する
+          value={this.state.value}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <TodoList list={this.state.list} />
       </div>
     );
   }
